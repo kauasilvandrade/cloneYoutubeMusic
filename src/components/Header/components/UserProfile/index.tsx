f66a2271d3782bf616c1a6b8a14d0c168ff6d817
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-03-19 11:04:28 -0300

    Componente UserProfile finalizado

--- src/components/Header/components/UserProfile/index.tsx
+++ src/components/Header/components/UserProfile/index.tsx
@@ -1,9 +1,18 @@
 import styles from "./styles.module.css"
 
+import TransmissionIcon from "./../../../../assets/transmissionIcon.svg"
+import User from "./../../../../assets/user.jpg"
+
 export function UserProfile() {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <button>
+                <img src={TransmissionIcon} alt="Logo da Transmissão" />
+            </button>
+            <figure>
+                <img src={User} alt="User Profile" />
+                <figcaption>User</figcaption>
+            </figure>
         </div>
     )
 }
