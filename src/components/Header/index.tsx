849da4bd0c99b110d0ca5940d21303ec8ac3225b
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-03-19 13:12:30 -0300

    Animação do Scroll no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,32 @@
 import styles from "./styles.module.css"
 
+import { useEffect, useState } from "react"
+
 import { Logo } from "./components/Logo"
 import { Search } from "./components/Search"
 import { UserProfile } from "./components/UserProfile"
 
 export function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            if (window.scrollY > 10) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
     return (
-        <header className={styles.container}>
+        <header className={`${styles.container} ${scrolled ? styles.scrolled : ""}`}>
             <Logo />
             <Search />
             <UserProfile /> 
