318645911316eb871f39b54d2444d7c81912607c
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-03-19 10:39:57 -0300

    Componente Search finalizado

--- src/components/Header/components/Search/index.tsx
+++ src/components/Header/components/Search/index.tsx
@@ -1,9 +1,19 @@
 import styles from "./styles.module.css"
 
+import { SearchIcon } from "lucide-react"
+
 export function Search() {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <button>
+                <SearchIcon color="#757575" size={18} />
+            </button>
+            <input 
+                type="text" 
+                name="search" 
+                id="search" 
+                placeholder="Pesquise músicas, álbuns, artistas, podcasts" 
+            />
         </div>
     )
 }
